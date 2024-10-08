4afd021673343cdc6fb4d3b9653f1a739dffa08d
Sumit1673 <sumitvaise29@gmail.com>
2024-10-08 14:30:28 +0000

    add tag for docker image

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,12 +6,12 @@ set -euo pipefail
 # docker build -f app/Dockerfile app/
 
 echo "Building Backend"
-docker build -f backend/Dockerfile backend/
+docker build  -t backend:v1 -f backend/Dockerfile backend/
 
 echo "Running Backend"
 docker run --rm -p 8081:8081 backend:v1
 
-# echo "Testing the application"
+echo "Testing the application"
 curl --header "Content-Type: application/json" \
         --request POST \
         --data '{"username":"xyz","password":"xyz"}' \
